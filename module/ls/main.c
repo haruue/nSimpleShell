687323d11c1d2deeec39542acb5851908e494a2f
Haruue Icymoon <haruue@caoyue.com.cn>
2016-06-12 23:37:43 +0800

    改用正常的方法实现 ls

--- module/ls/main.c
+++ module/ls/main.c
@@ -1,10 +1,8 @@
 #include <libndls.h>
 #include <module.h>
 #include <dir_common.h>
+#include <dirent.h>
 
-
-int list_files(const char *path, void *context);
-char *cut_path(const char *file_path, const char *dir_path);
 
 int module_main(int argc, char *argv[]) {
 	char *path = malloc(MAX_PATH_LENGTH * sizeof(char));
@@ -17,35 +15,16 @@ int module_main(int argc, char *argv[]) {
 		nio_printf("ls: cannot access %s: No such directory\n", argv[1]);
 		return -1;
 	}
-	int (*callback_list_files)(const char *path, void *context);
-	callback_list_files = list_files;
-	file_each(path, callback_list_files, path);
+	// Read dir
+	DIR *dir;
+	dir = opendir(path);
+	struct dirent * ptr;
+	while ((ptr = readdir(dir)) != NULL) {
+		nio_printf("%s\t", ptr->d_name);
+	}
 	nio_printf("\n");
+	closedir(dir);
+
 	free(path);
 	return 0;
-}
-
-int list_files(const char *path, void *context) {
-	char *filename = cut_path(path, (char *) context);
-	if (filename != NULL) {
-		nio_printf("%s\t", filename);
-	}
-	return 0;
-}
-
-/**
- * 处理递归时的 path ，截取指定目录下的路径
- */
-char *cut_path(const char *file_path, const char *dir_path) {
-	char *file_ptr = file_path;
-	char *dir_ptr = dir_path;
-	while (*++dir_ptr == *++file_ptr);
-	if (*file_ptr == '/') {
-		file_ptr++;
-	}
-	if (strstr(file_ptr, "/") == NULL) {
-		return file_ptr;
-	} else {
-		return NULL;
-	}
 }
